7f80956ade63a0d0c9f24a1ba73258704274b775
shnakamura <joaovtnakamura@gmail.com>
2026-07-07 07:30:43 -0300

    refactor: quest log player

---
+++ src/QuestBooks/Common/Commands/QuestDesignerCommand.cs
@@ -0,0 +1,25 @@
+﻿using Terraria.Audio;
+
+namespace QuestBooks.Common.Commands;
+
+internal sealed class QuickDesignerCommand : ModCommand
+{
+    public override string Command => "questdesigner";
+
+    public override CommandType Type => CommandType.Chat;
+
+    public override void Action(CommandCaller caller, string input, string[] args)
+    {
+        QuestBooksMod.DesignerEnabled = !QuestBooksMod.DesignerEnabled;
+
+        if (QuestBooksMod.DesignerEnabled)
+        {
+            SoundEngine.PlaySound(SoundID.AchievementComplete);
+        }
+
+        else
+        {
+            SoundEngine.PlaySound(SoundID.Unlock);
+        }
+    }
+}
---
+++ src/QuestBooks/Core/Quests/QuestLogPlayer.cs
@@ -0,0 +1,60 @@
+﻿using QuestBooks.Common.Input;
+using Terraria.Audio;
+using Terraria.GameInput;
+
+namespace QuestBooks.Core.Quests;
+
+internal sealed class QuestLogPlayer : ModPlayer
+{
+    public override void ProcessTriggers(TriggersSet triggersSet)
+    {
+        if (!KeybindSystem.ToggleQuestLog.JustPressed)
+        {
+            return;
+        }
+        
+        QuestLogDrawer.Toggle();
+    }
+
+    public override void SetControls()
+    {
+        if (!QuestLogDrawer.DisplayLog || (!QuestLogDrawer.TargetDisplayLog && QuestLogDrawer.AnimationInProgress))
+        {
+            return;
+        }
+
+        if (Main.LocalPlayer.controlInv)
+        {
+            QuestLogDrawer.Toggle(false);
+            
+            Main.LocalPlayer.releaseInventory = false;
+        }
+        else if (Main.LocalPlayer.controlCreativeMenu && Main.LocalPlayer.difficulty == PlayerDifficultyID.Creative)
+        {
+            QuestLogDrawer.Toggle(false);
+            
+            Main.LocalPlayer.releaseCreativeMenu = false;
+        }
+    }
+
+    public override void PostUpdate()
+    {
+        if (!Main.LocalPlayer.dead || !QuestLogDrawer.DisplayLog)
+        {
+            return;
+        }
+        
+        QuestLogDrawer.Toggle(false);
+    }
+
+    public override void OnEnterWorld()
+    {
+        QuestLogDrawer.Toggle(false, true);
+
+        QuestLogDrawer.ActiveStyle.SelectedBook = null;
+        QuestLogDrawer.ActiveStyle.SelectedChapter = null;
+        QuestLogDrawer.ActiveStyle.SelectedElement = null;
+
+        QuestLogDrawer.ActiveStyle.OnEnterWorld();
+    }
+}

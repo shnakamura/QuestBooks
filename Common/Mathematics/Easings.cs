baf97505aa18a8346423f8fb58a64af1d7d94ff7
shnakamura <joaovtnakamura@gmail.com>
2026-09-03 05:26:25 -0300

    feat: easings

---
+++ Common/Mathematics/Easings.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace QuestBooks.Common.Mathematics;
+
+public static class Easings
+{
+    public static class Quadratic
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float In(float t) => t * t;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Out(float t) => 1f - In(1f - t);
+    }
+
+    public static class Cubic
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float In(float t) => t * t * t;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Out(float t) => 1f - In(1f - t);
+    }
+}
